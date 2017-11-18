d5510ffe74f0a0447379ed32a07df3cba3140bc9
suhaybabsi <suhaybal-absi@192.168.1.12>
2017-11-18 22:36:05 +0200

    slider-crank mechanism

--- README.md
+++ README.md
@@ -1,25 +1,13 @@
-# Magic-starter
+# Kinematics
 
-Starter kit to build graphical front-end tools using Javascript/ES6 and ReactJS.
-
-The default 2D graphics library is PaperJS. Tween.js is used as the main animation engine.
-
-Apps built using this kit could be easily deployed to cloud-hosting services such as <b>Heroku</b>.
-
-## Getting Ready
-
-Make sure to edit the following files to reflect your app description:
-
-* <code>package.json</code>: Preferably use <code>npm</code> command to add or remove packages.
-* <code>app.json</code>
-* <code>webpack.config.json</code>
+Workspace for visualizing basic machinery mechanisms.
 
 ### Running Locally (Development Mode)
 
-Run the following commands on terminal:
+Make sure you have [Node.js](https://nodejs.org) installed. Then run the following commands on terminal (Mac OS X)/Command Prompts (Windows):
 
 ```sh
-git clone https://github.com/suhaybimagine/imagination.git
+git clone https://github.com/suhaybabsi/kinematics.git
 npm install
 npm run dev
 ```
--- source/Main.js
+++ source/Main.js
@@ -1,21 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Paper from "paper";
+import Paper, { Point } from "paper";
 import TWEEN from "@tweenjs/tween.js";
-
-class Root extends React.Component {
-
-    render() {
-        return (
-            <div>
-                <h1>Hello World</h1>
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(<Root />, document.getElementById("app"));
 
 Paper.install(window);
 window.onload = function () {
@@ -32,11 +19,41 @@ function initCanvas() {
 
     console.log("PaperJS canvas is up and running !.");
 
-    let rect = new Paper.Path.Rectangle(new Rectangle(0, 0, 100, 4));
-    rect.fillColor = "red";
-    rect.position = new Point(100, 300);
-    
-    rect.on("frame", function (e) {
-        this.rotation += 3;
+    let crankLength = 130;
+    let crank = new Paper.Path.Rectangle(new Rectangle(0, 0, crankLength, 10));
+    crank.fillColor = "red";
+    crank.pivot = new Point(0, 5);
+    crank.position = new Point(300, 300);
+
+    let couplerLength = 200;
+    let coupler = new Paper.Path.Rectangle(new Rectangle(0, 0, couplerLength, 10));
+    coupler.fillColor = "green";
+    coupler.applyMatrix = false;
+    coupler.pivot = new Point(0, 5);
+    coupler.position = new Point(300, 300);
+
+    let slider = new Paper.Path.Rectangle(new Rectangle(0, 0, 50, 30));
+    slider.fillColor = "magenta";
+    slider.position = new Point(300, 300);
+
+    let offset = -40;
+    let tip = new Point(crankLength, 0);
+    let rotation = 0;
+    let speed = 2;
+
+    view.on("frame", function (e) {
+        rotation += speed;
+        crank.rotation += speed;
+
+        let cpl = crank.position.add(tip.rotate(rotation));
+        coupler.position = cpl;
+
+        let v1 = cpl.subtract(crank.position);
+        let acos = (v1.y -  + offset)/couplerLength;
+        let ang = Math.acos(acos);
+        let tx = cpl.x + Math.sin(ang) * couplerLength;
+
+        coupler.rotation =  - 90 + ang * 180 / Math.PI;
+        slider.position = new Point(tx, crank.position.y + offset);
     });
 }
